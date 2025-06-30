8216ec46bd02b5ceb6b452f927a194ab4948f130
Hong Kil Dong <hong@gamil.com>
2025-06-30 17:55:08 +0900

    2025-06-30 <Servlet - Mybatis>
    - Mybatis -
    Mybatis는 JDBC로 처리하는 상당부분의 코드와 파라미터 설정 및 결과 매핑을 대신해주는 프레임워크이다.
    
    - Mybatis 순서 -
    1. XML에서 SqlSessionFactory 빌드하기 (Config 설정)
        Config : Config 파일에서 db를 연결할 환경설정 및 Mapper 경로 설정
    
    2. 매핑된 SQL 구문 살펴보기 (Mapper 설정)
        Mapper : 쿼리문을 모아둔 파일
    
    3. SqlMapConfig 클래스 생성 (SqlSessionFactoryBuilder => SqlSessionFactory => SqlSession 순으로 진행)
    
    JDBC에 비해 훨씬 간편하게 코드를 작성하는 장점이 존재한다.

---
+++ SqlMapConfig.java
@@ -0,0 +1,45 @@
+package com.my.dao;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlMapConfig {
+	private SqlSessionFactory sqlSessionFactory;
+	
+	public SqlSessionFactory getSqlSessionFactory() {
+		
+		//Config파일의 경로
+		String resource = "com/my/db/Config.xml";
+		
+		try {
+			//Config.xml의 파일 정보를 읽어온다.
+			Reader reader = Resources.getResourceAsReader(resource);
+			
+			//읽어온 정보를 가지고 SqlSessionFactory객체 생성
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return sqlSessionFactory;
+	}
+}
+
+
+//SSFB(SqlSessionFactoryBuilder) -> SSF(SqlSessionFactory) -> SS(SqlSession) 
+
+
+
+
+
+
+
+
+
+
+
